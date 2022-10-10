15810563c97d9d4abf64e5f29c859595fadbe509
sbfffnico <nicoedrich91@gmail.com>
2022-10-10 01:06:47 -0500

    add react app project

---
+++ src/BodyComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function BodyComponent() {
+    return (
+        <main>
+            <p>In Great Britain, around 1824, a Royal Society for the Prevention of Cruelty to Animals was founded and pledged to not to wear feathers or furs from any creature that had not been killed for food.  Except for ostriches.  Apparently they had a vendetta against those guys.</p>
+        </main>
+    );
+}
+
+export default BodyComponent;
